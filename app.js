69054c79bb465e7ead18e859be2804d016046baa
Daniele Tortora <tortora87@gmail.com>
2019-12-12 10:52:02 +0000

    change message

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("This site is currently under construction, come back soon!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
